828e5aecf0625344ab84cfb209989667fef184de
Phạm Nguyễn Quốc Thanh <thanhwtf95@gmail.com>
2023-08-14 22:56:26 +0700

    add report

--- library.py
+++ library.py
@@ -1,17 +1,17 @@
-import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+# Assuming you have a DataFrame called 'data' with 'Degree' and 'Salary' columns
+# 'Degree' should have values 1 for Bachelor and 2 for Master
 
-# Load your dataset
-data = pd.read_csv('test.csv')
-
-# Group data by degree and calculate average salary
-degree_salary = data.groupby('Degree')['Salary'].mean()
+# Map degree values to labels
+data = pd.read_csv('train.csv')
+data['Degree_Label'] = data['Degree'].map({1: 'Bachelor', 2: 'Master'})
 
 # Create a bar plot
 plt.figure(figsize=(8, 6))
-degree_salary.plot(kind='bar', color='skyblue')
+sns.barplot(x='Degree_Label', y='Salary', data=data)
 plt.xlabel('Degree')
-plt.ylabel('Average Salary')
-plt.title('Average Salary by Degree')
-plt.xticks(rotation=0)
+plt.ylabel('Salary')
+plt.title('Bachelor vs. Master Salary')
 plt.show()
